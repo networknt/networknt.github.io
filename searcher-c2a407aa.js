6aa2e978924d5fc561b4efb98e2631def9a61e6f
stevehu <stevehu@users.noreply.github.com>
2026-02-25 14:58:26 +0000

    deploy: networknt/networknt-doc@f3f240fc6aecd8f23e55d96aa3fb0967bc6cb5f0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a1dad1de.js',
+                path_to_root + 'searchindex-fade165c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
